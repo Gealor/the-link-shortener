6f0215eac4ae8d6c7dddf3a039901fa9066ecbd9
Gealor <gzazulya@gmail.com>
2025-11-20 22:37:41 +0400

    add logger

---
+++ src/logger.py
@@ -0,0 +1,10 @@
+import logging
+from core.config import settings
+
+logging.basicConfig(
+    format=settings.logger.LOG_DEFAULT_FORMAT,
+    datefmt=settings.logger.datefmt,
+    level=settings.logger.level,
+)
+
+log = logging.getLogger(__name__)
