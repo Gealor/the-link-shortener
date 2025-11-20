d21ba6f78c01b32d00832f138cabd615186b462c
Gealor <gzazulya@gmail.com>
2025-11-20 22:37:36 +0400

    add log settings

--- src/core/config.py
+++ src/core/config.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -6,6 +7,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 BASE_DIR = Path(__file__).parent.parent
 ENV_FILE = BASE_DIR / '.env'
 ENV_TEMPLATE = BASE_DIR / '.env.template'
+
+class LogSettings(BaseModel):
+    LOG_DEFAULT_FORMAT: str = (
+        "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+    )
+    level: int = logging.INFO
+    datefmt: str = "%Y-%m-%d %H:%M:%S"
 
 class RuntimeSettings(BaseModel):
     host: str = '0.0.0.0'
@@ -18,6 +26,7 @@ class Settings(BaseSettings):
     )
 
     runtime: RuntimeSettings = RuntimeSettings()
+    logger: LogSettings = LogSettings()
 
 
 settings = Settings()
