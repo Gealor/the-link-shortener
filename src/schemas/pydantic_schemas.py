683c76e80a6452aad18e6b88fa973f7e8a5dd585
Gealor <gzazulya@gmail.com>
2025-11-23 15:04:56 +0400

    pydantic schema fix

--- src/schemas/pydantic_schemas.py
+++ src/schemas/pydantic_schemas.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, computed_field
+from core.config import settings
 
 
 class URLShort(BaseModel):
@@ -6,3 +7,7 @@ class URLShort(BaseModel):
 
     full_url: str = Field(examples=["https://google.com/?something=2345676812"])
     slug: str = Field(examples=["9DwYb3"])
+
+    @computed_field
+    def full_slug(self) -> str:
+        return f"{settings.runtime.protocol}://{settings.runtime.host}:{settings.runtime.port}/{self.slug}"
