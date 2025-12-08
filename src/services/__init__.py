b70ef5311e99475dccf8b5f13490950d3eb00f46
Gealor <gzazulya@gmail.com>
2025-12-08 21:25:02 +0400

    dependency ShortenerService

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,3 +1,12 @@
 __all__ = ("ShortenerService")
 
+from fastapi import Depends
+
+from core.database import db_session_getter
 from .shortener_service import ShortenerService
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+# Depends ShortenerService
+def get_shortener_service(session: AsyncSession = Depends(db_session_getter)):
+    return ShortenerService(session=session)
