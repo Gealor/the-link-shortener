3e3438daddba79fd97faf646164f77274e2d77be
Gealor <gzazulya@gmail.com>
2025-12-08 21:24:10 +0400

    add custom pydantic schemas

--- src/schemas/pydantic_schemas.py
+++ src/schemas/pydantic_schemas.py
@@ -1,13 +1,23 @@
-from pydantic import BaseModel, ConfigDict, Field, computed_field
+from pydantic import BaseModel, ConfigDict, Field, HttpUrl, computed_field
 from core.config import settings
 
+class BaseURL(BaseModel):
+    full_url: HttpUrl = Field(examples=["https://google.com/?something=2345676812"])
 
-class URLShort(BaseModel):
+class BodyCreateSlug(BaseURL):
+    pass
+
+class URLShort(BaseURL):
     model_config = ConfigDict(from_attributes=True)
-
-    full_url: str = Field(examples=["https://google.com/?something=2345676812"])
+    
     slug: str = Field(examples=["9DwYb3"])
 
     @computed_field
     def full_slug(self) -> str:
-        return f"{settings.runtime.protocol}://{settings.runtime.host}:{settings.runtime.port}/{self.slug}"
+        return f"{settings.full_slug.protocol}://{settings.full_slug.host}:{settings.runtime.port}/{self.slug}"
+
+
+
+class CreateShortURL(BaseModel):
+    full_url: str = Field(examples=["https://google.com/?something=2345676812"])
+    slug: str = Field(examples=["9DwYb3"])
