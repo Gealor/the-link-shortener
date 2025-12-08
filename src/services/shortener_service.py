1da256a778b9d77b71ec5d220cccb05ef279b99d
Gealor <gzazulya@gmail.com>
2025-12-08 21:24:43 +0400

    update ShortenerService

--- src/services/shortener_service.py
+++ src/services/shortener_service.py
@@ -1,28 +1,54 @@
-import asyncio
 from random import choice
 import string
+
+from pydantic import HttpUrl
+
 from logger import log
-from schemas.pydantic_schemas import URLShort
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from repositories.shortener_url_repository import ShortenerURLRepository
+from schemas.exceptions import SlugAlreadyExistsException
+from schemas.pydantic_schemas import CreateShortURL, URLShort
+from utils.decorators import repeat_decorator
 
 ALPHABET: str = string.ascii_letters + string.digits
 
 class ShortenerService:
-    def __init__(self):
-        pass
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        self.repo = ShortenerURLRepository(session=self.session)
 
     def _generate_random_code(self) -> str:
         return "".join([choice(ALPHABET) for _ in range(6)])
     
-    async def generate_and_safe_shortcut_url(self, url: str) -> URLShort:
+    @repeat_decorator()
+    async def _generate_code_without_repeating(self) -> str:
         code = self._generate_random_code()
-
-        log.info("Generated slug %s for url: %s", code, url)
-
-        await asyncio.sleep(1)
-        log.info("Write in DB alias")
-        return URLShort(
-            full_url=url, 
-            slug=code,
+        found_record = await self.repo.get_record_by_slug(code)
+        if found_record and found_record.slug == code:
+            raise SlugAlreadyExistsException("This slug already exist.")
+        return code
+    
+    async def generate_and_safe_shortcut_url(self, url: HttpUrl) -> URLShort:
+        str_url = str(url)
+        if record := await self.repo.get_record_by_full_url(full_url=str_url):
+            log.info("Slug with these url already exist. Return existed record")
+            return URLShort(
+                full_url=url, 
+                slug=record.slug,
+            )
+        
+        code = await self._generate_code_without_repeating()
+        
+        created_record = CreateShortURL(
+            full_url=str_url,
+            slug=code
         )
+        record = await self.repo.create_record(short_url=created_record)
+        log.info("Generated slug %s for url: %s", code, url)
+    
+        return URLShort.model_validate(record)
+
+
 
 
