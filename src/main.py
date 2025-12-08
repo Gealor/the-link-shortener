5b88eeaaab3e581424b09be370c5a9b47a4f861b
Gealor <gzazulya@gmail.com>
2025-12-08 21:29:16 +0400

    add many addresses for CORS

--- src/main.py
+++ src/main.py
@@ -14,6 +14,10 @@ app = FastAPI()
 origins = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost",
+    "http://127.0.0.1",
 ] # адреса, где расположен фронтэнд 
 
 app.add_middleware(
