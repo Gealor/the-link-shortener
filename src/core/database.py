e8b55f2e51d9df93eb2f678f8cce43c35797054c
Gealor <gzazulya@gmail.com>
2025-12-08 21:22:56 +0400

    change engine on async and added some params for sessionmaker

--- src/core/database.py
+++ src/core/database.py
@@ -1,15 +1,18 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
-from sqlalchemy import create_engine, Engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from .config import settings
 
 
-engine = create_engine(
+engine = create_async_engine(
     url=settings.database.db_url,
     echo=settings.database.db_echo,
 )
 
-async_session_maker = async_sessionmaker(bind=engine)
+async_session_maker = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    autoflush=False
+)
 
 async def db_session_getter():
     async with async_session_maker() as session:
