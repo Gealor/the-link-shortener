71ca015e2696d6af9f87ceeb8f1e2e89bd5b42e1
Gealor <gzazulya@gmail.com>
2025-12-16 00:07:04 +0400

    refactor config

--- src/core/config.py
+++ src/core/config.py
@@ -1,9 +1,11 @@
 import logging
 from pathlib import Path
 from typing import Annotated
-from pydantic import BaseModel, Field
-from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from pydantic import BaseModel
+from pydantic import Field
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
 
 BASE_DIR = Path(__file__).parent.parent
 ENV_FILE = BASE_DIR / '.env'
@@ -40,10 +42,10 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=(ENV_TEMPLATE, ENV_FILE),
         case_sensitive=False, # чувствительность к регистру
-        env_nested_delimiter="__", # обязательный параметр, если настройки поделены на разные классы, 
-    # в этом случае в .env у параметров указывается сначала имя переменной, в которой лежат переменные в python коде, 
-    # потом уже этот разделитель __ и после этого уже имя переменной окружения, 
-    # т.е. если мы в главном классе настроек Settings создали атрибут database, 
+        env_nested_delimiter="__", # обязательный параметр, если настройки поделены на разные классы,
+    # в этом случае в .env у параметров указывается сначала имя переменной, в которой лежат переменные в python коде,
+    # потом уже этот разделитель __ и после этого уже имя переменной окружения,
+    # т.е. если мы в главном классе настроек Settings создали атрибут database,
     # то в .env переменные окружения должны начинаться с DATABASE__(либо database__, если у нас case_sensitive=True)
     )
 
